24a2a28b035c17f90bc87e5d01b49fdff645f620
Kagev <kaplitskiygv@gmail.com>
2023-05-11 12:21:35 +0300

    0.0.0

--- cli_bot_v3.py
+++ cli_bot_v3.py
@@ -19,22 +19,22 @@ class Phone(Field):
 
 class Record:
     def __init__(self, name):
-        self.name = Name(name)
+        self.name = name
         self.phones = []
 
     def add_phone(self, phone):
-        self.phones.append(Phone(phone))
+        self.phones.append(phone)
 
     def remove_phone(self, phone):
         for p in self.phones:
-            if p.value == phone:
+            if p.value == phone.value:
                 self.phones.remove(p)
                 return
 
     def edit_phone(self, old_phone, new_phone):
         for p in self.phones:
-            if p.value == old_phone:
-                p.value = new_phone
+            if p.value == old_phone.value:
+                p.value = new_phone.value
                 return
 
     def __repr__(self):
@@ -43,7 +43,7 @@ class Record:
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
-
-
-# book = AddressBook()
+    
+    def get_record(self, name):
+        return self.data[name]
 
